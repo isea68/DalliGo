fe782f3453b1fc174475051bbbed455a7023057e
kimjisoo <you@example.com>
2025-12-03 15:49:22 +0900

    feat:1203

--- src/main/java/com/human/dalligo/controller/RRenrollController.java
+++ src/main/java/com/human/dalligo/controller/RRenrollController.java
@@ -4,8 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 
-
-import com.human.dalligo.vo.UserVO;
+import com.human.dalligo.vo.JSUserVO;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -16,7 +15,7 @@ public class RRenrollController {
 	@PostMapping("/enrolluser")
 	public String postEnroll(HttpSession session,Model model) {
 		
-		UserVO loginUser = (UserVO)session.getAttribute("loginUser");
+		JSUserVO loginUser = (JSUserVO)session.getAttribute("loginUser");
 		System.out.println("user:"+loginUser.getName());
 
 		model.addAttribute("user", loginUser);		
